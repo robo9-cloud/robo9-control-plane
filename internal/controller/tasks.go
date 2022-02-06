97a93ac9cbcf6055f4d1896756249157cbecdd40
Zak Hassan <zak.hassan1010@gmail.com>
2022-02-05 23:10:44 -0800

    Adding uber logging library zap to log data.

--- internal/controller/tasks.go
+++ internal/controller/tasks.go
@@ -2,9 +2,9 @@ package controller
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/serverless-cloud-robot/robo9/internal/api"
+	"go.uber.org/zap"
 	"net/http"
 )
 
@@ -18,27 +18,27 @@ func (tc *taskController) GetAll(w http.ResponseWriter, r *http.Request) {
 func (tc *taskController) Put(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	id := params["id"]
-	fmt.Println(id)
+	zap.S().Infof("ID %v", id)
 
 	t, err := tc.parseRequest(r)
 	if err != nil {
+		zap.S().Info(err.Error())
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte("Could not parse Task object"))
-		fmt.Println(err.Error())
 		return
 	}
 	if id != t.ID {
+		zap.S().Info(err.Error())
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte("ID of submitted Task must match ID of URL"))
-		fmt.Println(err.Error())
 		return
 	}
 
 	t, err = api.UpdateTask(t)
 	if err != nil {
+		zap.S().Info(err.Error())
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte(err.Error()))
-		fmt.Println(err.Error())
 		return
 	}
 	encodeResponseAsJSON(t, w)
@@ -46,15 +46,15 @@ func (tc *taskController) Put(w http.ResponseWriter, r *http.Request) {
 
 func (tc *taskController) Post(w http.ResponseWriter, r *http.Request) {
 	u, err := tc.parseRequest(r) // TODO: Implement this function
-	fmt.Println("Parsed :")
-	fmt.Println(u)
 	if err != nil {
+		zap.S().Info(err.Error())
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte("Could not parse Task object"))
 		return
 	}
 	u, err = api.AddTask(u)
 	if err != nil {
+		zap.S().Info(err.Error())
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(err.Error()))
 		return
@@ -66,8 +66,8 @@ func (tc *taskController) parseRequest(r *http.Request) (api.Task, error) {
 	dec := json.NewDecoder(r.Body)
 	var t api.Task
 	err := dec.Decode(&t)
-	fmt.Println(t)
 	if err != nil {
+		zap.S().Info(err.Error())
 		return api.Task{}, err
 	}
 	return t, nil
@@ -76,9 +76,10 @@ func (tc *taskController) parseRequest(r *http.Request) (api.Task, error) {
 func (tc *taskController) Get(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	id := params["id"]
-	fmt.Println(id)
-	u, err := api.GetTaskByID(id) // TODO: Need to implement this function
+	zap.S().Infof("ID %v", id)
+	u, err := api.GetTaskByID(id)
 	if err != nil {
+		zap.S().Info(err.Error())
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
@@ -89,8 +90,10 @@ func (tc *taskController) Get(w http.ResponseWriter, r *http.Request) {
 func (tc *taskController) Delete(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	id := params["id"]
+	zap.S().Infof("ID %v", id)
 	err := api.RemoveTaskById(id)
 	if err != nil {
+		zap.S().Info(err.Error())
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(err.Error()))
 		return
--- main.go
+++ main.go
@@ -5,12 +5,14 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package main
 
 import (
-	"fmt"
 	"github.com/serverless-cloud-robot/robo9/internal/controller"
+	"go.uber.org/zap"
 )
 
 func main() {
-	fmt.Println("starting api web service. v0.0.1 ...")
+	logger, _ := zap.NewDevelopment()
+	zap.ReplaceGlobals(logger)
+	zap.S().Info("starting api web service. v0.0.1 ...")
 	controller.StartServer()
 
 }
